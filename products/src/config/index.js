5e3a92bd5b0ed282c26f22b4444a8eaef84287e4
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-08-07 10:09:45 -0300

    nginx dockerfile

--- products/src/config/index.js
+++ products/src/config/index.js
@@ -1,11 +1,14 @@
 const dotEnv = require('dotenv');
 
-if (process.env.NODE_ENV !== 'prod') {
+if (process.env.NODE_ENV !== 'production') {
   const configFile = `./.env.${process.env.NODE_ENV}`;
   dotEnv.config({ path: configFile });
 } else {
   dotEnv.config();
 }
+
+console.log("DB",process.env.MONGODB_URI)
+console.log('NODE_ENV',process.env.NODE_ENV)
 
 module.exports = {
   PORT: process.env.PORT,
